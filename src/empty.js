535abad2d3a6f15d2c96726b1e5bb8b0612ffa50
Ruben Hazelaar <ruben.hazelaar@gmail.com>
2016-10-02 17:58:34 -0300

    fix: Fixed empty()

--- src/empty.js
+++ src/empty.js
@@ -1,6 +1,6 @@
 export default function empty(Element):Node {
     while (Element.lastChild) {
-        this.removeChild(Element.lastChild);
+        Element.removeChild(Element.lastChild);
     }
     return Element;
 }
